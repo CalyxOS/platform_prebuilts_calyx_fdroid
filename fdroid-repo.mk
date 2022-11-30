4b0403003664bc9bcfd37455996942caf6c148cc
Chirayu Desai <chirayudesai1@gmail.com>
2022-12-01 02:39:40 +0530

    Update fdroid repo to 3404821451
    Change-Id: I81c95ff5f7f0e5e4e3e2e16dae30fbb9e20df195

--- fdroid-repo.mk
+++ fdroid-repo.mk
@@ -10,37 +10,34 @@ PRODUCT_COPY_FILES += \
     prebuilts/calyx/fdroid/repo/com.fsck.k9/en-US/icon.png:$(TARGET_COPY_OUT_PRODUCT)/fdroid/repo/com.fsck.k9/en-US/icon.png \
     prebuilts/calyx/fdroid/repo/com.jarsilio.android.scrambledeggsif/en-US/icon.png:$(TARGET_COPY_OUT_PRODUCT)/fdroid/repo/com.jarsilio.android.scrambledeggsif/en-US/icon.png \
     prebuilts/calyx/fdroid/repo/com.nextcloud.client/en-US/icon.png:$(TARGET_COPY_OUT_PRODUCT)/fdroid/repo/com.nextcloud.client/en-US/icon.png \
+    prebuilts/calyx/fdroid/repo/entry.jar:$(TARGET_COPY_OUT_PRODUCT)/fdroid/repo/entry.jar \
+    prebuilts/calyx/fdroid/repo/entry.json:$(TARGET_COPY_OUT_PRODUCT)/fdroid/repo/entry.json \
     prebuilts/calyx/fdroid/repo/icons-120/org.calyxinstitute.vpn.160000.png:$(TARGET_COPY_OUT_PRODUCT)/fdroid/repo/icons-120/org.calyxinstitute.vpn.160000.png \
-    prebuilts/calyx/fdroid/repo/icons-120/org.dmfs.tasks.82200.png:$(TARGET_COPY_OUT_PRODUCT)/fdroid/repo/icons-120/org.dmfs.tasks.82200.png \
     prebuilts/calyx/fdroid/repo/icons-120/se.leap.riseupvpn.162000.png:$(TARGET_COPY_OUT_PRODUCT)/fdroid/repo/icons-120/se.leap.riseupvpn.162000.png \
     prebuilts/calyx/fdroid/repo/icons-160/org.calyxinstitute.vpn.160000.png:$(TARGET_COPY_OUT_PRODUCT)/fdroid/repo/icons-160/org.calyxinstitute.vpn.160000.png \
-    prebuilts/calyx/fdroid/repo/icons-160/org.dmfs.tasks.82200.png:$(TARGET_COPY_OUT_PRODUCT)/fdroid/repo/icons-160/org.dmfs.tasks.82200.png \
     prebuilts/calyx/fdroid/repo/icons-160/se.leap.riseupvpn.162000.png:$(TARGET_COPY_OUT_PRODUCT)/fdroid/repo/icons-160/se.leap.riseupvpn.162000.png \
     prebuilts/calyx/fdroid/repo/icons-240/org.calyxinstitute.vpn.160000.png:$(TARGET_COPY_OUT_PRODUCT)/fdroid/repo/icons-240/org.calyxinstitute.vpn.160000.png \
-    prebuilts/calyx/fdroid/repo/icons-240/org.dmfs.tasks.82200.png:$(TARGET_COPY_OUT_PRODUCT)/fdroid/repo/icons-240/org.dmfs.tasks.82200.png \
     prebuilts/calyx/fdroid/repo/icons-240/se.leap.riseupvpn.162000.png:$(TARGET_COPY_OUT_PRODUCT)/fdroid/repo/icons-240/se.leap.riseupvpn.162000.png \
     prebuilts/calyx/fdroid/repo/icons-320/org.calyxinstitute.vpn.160000.png:$(TARGET_COPY_OUT_PRODUCT)/fdroid/repo/icons-320/org.calyxinstitute.vpn.160000.png \
-    prebuilts/calyx/fdroid/repo/icons-320/org.dmfs.tasks.82200.png:$(TARGET_COPY_OUT_PRODUCT)/fdroid/repo/icons-320/org.dmfs.tasks.82200.png \
     prebuilts/calyx/fdroid/repo/icons-320/se.leap.riseupvpn.162000.png:$(TARGET_COPY_OUT_PRODUCT)/fdroid/repo/icons-320/se.leap.riseupvpn.162000.png \
     prebuilts/calyx/fdroid/repo/icons-480/org.calyxinstitute.vpn.160000.png:$(TARGET_COPY_OUT_PRODUCT)/fdroid/repo/icons-480/org.calyxinstitute.vpn.160000.png \
-    prebuilts/calyx/fdroid/repo/icons-480/org.dmfs.tasks.82200.png:$(TARGET_COPY_OUT_PRODUCT)/fdroid/repo/icons-480/org.dmfs.tasks.82200.png \
     prebuilts/calyx/fdroid/repo/icons-480/se.leap.riseupvpn.162000.png:$(TARGET_COPY_OUT_PRODUCT)/fdroid/repo/icons-480/se.leap.riseupvpn.162000.png \
     prebuilts/calyx/fdroid/repo/icons-640/org.calyxinstitute.vpn.160000.png:$(TARGET_COPY_OUT_PRODUCT)/fdroid/repo/icons-640/org.calyxinstitute.vpn.160000.png \
-    prebuilts/calyx/fdroid/repo/icons-640/org.dmfs.tasks.82200.png:$(TARGET_COPY_OUT_PRODUCT)/fdroid/repo/icons-640/org.dmfs.tasks.82200.png \
     prebuilts/calyx/fdroid/repo/icons-640/se.leap.riseupvpn.162000.png:$(TARGET_COPY_OUT_PRODUCT)/fdroid/repo/icons-640/se.leap.riseupvpn.162000.png \
     prebuilts/calyx/fdroid/repo/icons/fdroid-icon.png:$(TARGET_COPY_OUT_PRODUCT)/fdroid/repo/icons/fdroid-icon.png \
     prebuilts/calyx/fdroid/repo/icons/org.calyxinstitute.vpn.160000.png:$(TARGET_COPY_OUT_PRODUCT)/fdroid/repo/icons/org.calyxinstitute.vpn.160000.png \
-    prebuilts/calyx/fdroid/repo/icons/org.dmfs.tasks.82200.png:$(TARGET_COPY_OUT_PRODUCT)/fdroid/repo/icons/org.dmfs.tasks.82200.png \
     prebuilts/calyx/fdroid/repo/icons/se.leap.riseupvpn.162000.png:$(TARGET_COPY_OUT_PRODUCT)/fdroid/repo/icons/se.leap.riseupvpn.162000.png \
+    prebuilts/calyx/fdroid/repo/index.css:$(TARGET_COPY_OUT_PRODUCT)/fdroid/repo/index.css \
+    prebuilts/calyx/fdroid/repo/index.html:$(TARGET_COPY_OUT_PRODUCT)/fdroid/repo/index.html \
     prebuilts/calyx/fdroid/repo/index.jar:$(TARGET_COPY_OUT_PRODUCT)/fdroid/repo/index.jar \
+    prebuilts/calyx/fdroid/repo/index.png:$(TARGET_COPY_OUT_PRODUCT)/fdroid/repo/index.png \
     prebuilts/calyx/fdroid/repo/index-v1.jar:$(TARGET_COPY_OUT_PRODUCT)/fdroid/repo/index-v1.jar \
     prebuilts/calyx/fdroid/repo/index-v1.json:$(TARGET_COPY_OUT_PRODUCT)/fdroid/repo/index-v1.json \
+    prebuilts/calyx/fdroid/repo/index-v2.json:$(TARGET_COPY_OUT_PRODUCT)/fdroid/repo/index-v2.json \
     prebuilts/calyx/fdroid/repo/index.xml:$(TARGET_COPY_OUT_PRODUCT)/fdroid/repo/index.xml \
     prebuilts/calyx/fdroid/repo/org.briarproject.briar.android/en-US/icon.png:$(TARGET_COPY_OUT_PRODUCT)/fdroid/repo/org.briarproject.briar.android/en-US/icon.png \
     prebuilts/calyx/fdroid/repo/org.calyxinstitute.vpn/en-US/icon.png:$(TARGET_COPY_OUT_PRODUCT)/fdroid/repo/org.calyxinstitute.vpn/en-US/icon.png \
-    prebuilts/calyx/fdroid/repo/org.dmfs.tasks/en-US/icon.png:$(TARGET_COPY_OUT_PRODUCT)/fdroid/repo/org.dmfs.tasks/en-US/icon.png \
     prebuilts/calyx/fdroid/repo/org.openobservatory.ooniprobe/en-US/icon.png:$(TARGET_COPY_OUT_PRODUCT)/fdroid/repo/org.openobservatory.ooniprobe/en-US/icon.png \
-    prebuilts/calyx/fdroid/repo/org.secuso.privacyfriendlyweather/en-US/icon.png:$(TARGET_COPY_OUT_PRODUCT)/fdroid/repo/org.secuso.privacyfriendlyweather/en-US/icon.png \
     prebuilts/calyx/fdroid/repo/org.thoughtcrime.securesms/en-US/icon.png:$(TARGET_COPY_OUT_PRODUCT)/fdroid/repo/org.thoughtcrime.securesms/en-US/icon.png \
     prebuilts/calyx/fdroid/repo/org.torproject.android/en-US/icon.png:$(TARGET_COPY_OUT_PRODUCT)/fdroid/repo/org.torproject.android/en-US/icon.png \
     prebuilts/calyx/fdroid/repo/org.torproject.torbrowser/en-US/icon.png:$(TARGET_COPY_OUT_PRODUCT)/fdroid/repo/org.torproject.torbrowser/en-US/icon.png \
@@ -54,6 +51,7 @@ PRODUCT_PACKAGES += \
     Briar \
     CalyxVPN \
     DAVx5 \
+    GeometricWeather \
     K-9Mail \
     MuPDFviewer \
     Nextcloud \
@@ -63,6 +61,4 @@ PRODUCT_PACKAGES += \
     RiseupVPN \
     ScrambledExif \
     Signal \
-    Tasks \
     TorBrowser \
-    Weather \
